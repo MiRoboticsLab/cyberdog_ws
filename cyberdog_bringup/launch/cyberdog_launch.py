4325dcba9e6832db2d4a3ad3eb64fd9b18d8ccef
shangzihan <shangzihan@xiaomi.com>
2022-03-22 16:10:01 +0800

    [add] add sensor_manager launch and null file.
    null
    
    1. 添加sensor_managerlaunch；
    2. 添加空文件适配架构。

--- cyberdog_bringup/launch/cyberdog_launch.py
+++ cyberdog_bringup/launch/cyberdog_launch.py
@@ -11,9 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from doctest import OutputChecker
 from launch import LaunchDescription
-import launch
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import yaml
