86b491a0e37da1cd7266b53c06634b5a660d51ab
田洪海 <tianhonghai@126.com>
2022-03-25 11:23:24 +0800

    [Modify]: add sh for commitid sum of all repos

--- tools/build_arm.sh
+++ tools/build_arm.sh
@@ -3,34 +3,35 @@
 set -e
 
 current_path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-ROOTDIR=/
-BUILDER_OUTDIR=/home/builder
-CARPO_DEB=carpo_deb_repos
-CARPO_DEBPATH=${ROOTDIR}${CARPO_DEB}
-ROS2_GALACTIC_PATH=${ROOTDIR}opt/ros2/galactic
-ROS2_CYBERDOG_PATH=${ROOTDIR}opt/ros2/cyberdog
-ROS2_WS_PATH=${ROOTDIR}carpo_ws
+ROS2_GALACTIC_PATH=/opt/ros2/galactic
+ROS2_CYBERDOG_PATH=/opt/ros2/cyberdog
 DATETIME=$(date "+%Y-%m-%d_%H-%M-%S")
 
-echo -n "clone cyberdog ros2 source code"
-mkdir -p ${ROS2_WS_PATH}/src
-cd ${ROS2_WS_PATH}/src
+echo -n "clone carpo_cyberdog_ros2_lib_deb directories"
+git clone git@git.n.xiaomi.com:MiRoboticsLab/deb_workspace/carpo_deb_ws/carpo_cyberdog_ros2_lib_deb.git
+rm carpo_cyberdog_ros2_lib_deb/.git -rf
+
+source /opt/ros2/galactic/local_setup.bash
+
+echo -n "clone cyberdog ros2 applications source code"
+mkdir -p /carpo_ws/src
+cd /carpo_ws/src
 git clone git@git.n.xiaomi.com:MiRoboticsLab/rop/cyberdog_ws.git
-vcs import . < cyberdog_ws/cyberdog.repos 
+vcs import . < cyberdog_ws/cyberdog.repos
 cd ..
 
-echo -n "build cyberdog ros2 source code"
-source ${ROS2_GALACTIC_PATH}/local_setup.bash
-rm ${ROS2_CYBERDOG_PATH}/* -rf
-colcon build --merge-install --install-base ${ROS2_CYBERDOG_PATH} --parallel-workers 40
+echo -n "build cyberdog ros2 applications source code"
+colcon build --merge-install --install-base /opt/ros2/cyberdog --parallel-workers 40 --packages-select=protocol
 
-echo -n "package cyberdog ros2 source code"
-mkdir ${CARPO_DEBPATH}
-cd ${CARPO_DEBPATH}
-git clone git@git.n.xiaomi.com:MiRoboticsLab/deb_workspace/carpo_deb_ws/carpo_cyberdog_ros2_lib_deb.git
-rm carpo_cyberdog_ros2_lib_deb/.git/ -rf
-cp ${ROS2_CYBERDOG_PATH}/* ${CARPO_DEBPATH}/carpo_cyberdog_ros2_lib_deb/src${ROS2_CYBERDOG_PATH} -rf
+echo -n "get commit id sum of all repos"
+cd /
+git clone git@git.n.xiaomi.com:MiRoboticsLab/os/toolbox/carpo_deb_repos.git
+cd carpo_deb_repos/version_manager_tools
+./get_commitid_sum.sh ros_platform_code.xml carpo-cyberdog-ros2-lib
+cp ros_platform_code.commitidsum /
 
-tar -czf ${BUILDER_OUTDIR}/${CARPO_DEB}.tgz ${CARPO_DEBPATH}
+cd /
+cp /opt/ros2/cyberdog/* /carpo_cyberdog_ros2_lib_deb/src/opt/ros2/cyberdog/ -rf
+
+tar -czf /home/builder/carpo_deb_repos.tgz /carpo_cyberdog_ros2_lib_deb/ ros_platform_code.commitidsum
 echo finished!
-
