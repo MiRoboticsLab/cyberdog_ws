43032c89f59fae0a939b2b72e489aa59053ec8f6
shangzihan <shangzihan@xiaomi.com>
2022-04-15 11:49:12 +0800

    [fix] fix python import.
    null
    
    1. 优化 python import。

--- cyberdog_bringup/bringup/bringup.py
+++ cyberdog_bringup/bringup/bringup.py
@@ -12,13 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import getopt
 import os
 import re
 import sys
-import getopt
-import time
-import socket
 import platform
+import socket
+import time
 import uuid
 
 
