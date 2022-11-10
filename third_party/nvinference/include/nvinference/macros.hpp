af24d76bbcf03f342b218413ba002586ae59f36b
liangxiaowei <liangxiaowei1@xiaomi.com>
2022-11-10 16:37:43 +0800

    [add]: add cyberdog_action nvinference dependence in thirdparty
      NONE
    
      1.加天cyberdog_action的第三方依赖库，nvinference
      2. 将cyberdog_action 加入到自启动

---
+++ third_party/nvinference/include/nvinference/macros.hpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2021 Beijing Xiaomi Mobile Software Co., Ltd. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CYBERDOG_ACTION__MACROS_HPP_
+#define CYBERDOG_ACTION__MACROS_HPP_
+
+#if NV_TENSORRT_MAJOR >= 8
+#define TRT_NOEXCEPT noexcept
+#define TRT_CONST_ENQUEUE const
+#else
+#define TRT_NOEXCEPT
+#define TRT_CONST_ENQUEUE
+#endif
+
+#endif  // CYBERDOG_ACTION__MACROS_HPP_
